abecbbd85dec8322879b147f4a0aba34413bed81
Yeferson <yguerraq@unjbg.edu.pe>
2025-12-15 22:53:55 -0500

    Correcion de error del entregable 1

--- syscall.c
+++ syscall.c
@@ -13,9 +13,41 @@ int trace_active = 0;
  * ================================
  * CONTADOR DE SYSCALLS (ENTREGABLE 3)
  * ================================
- * Cada índice representa el número de syscall
  */
 int syscall_count[NSYSCALLS];
+
+/*
+ * ================================
+ * NOMBRES DE SYSCALLS (ENTREGABLE 1)
+ * ================================
+ */
+static char *syscall_names[NSYSCALLS] = {
+  [SYS_fork]    "fork",
+  [SYS_exit]    "exit",
+  [SYS_wait]    "wait",
+  [SYS_pipe]    "pipe",
+  [SYS_read]    "read",
+  [SYS_kill]    "kill",
+  [SYS_exec]    "exec",
+  [SYS_fstat]   "fstat",
+  [SYS_chdir]   "chdir",
+  [SYS_dup]     "dup",
+  [SYS_getpid]  "getpid",
+  [SYS_sbrk]    "sbrk",
+  [SYS_sleep]   "sleep",
+  [SYS_uptime]  "uptime",
+  [SYS_open]    "open",
+  [SYS_write]   "write",
+  [SYS_mknod]   "mknod",
+  [SYS_unlink]  "unlink",
+  [SYS_link]    "link",
+  [SYS_mkdir]   "mkdir",
+  [SYS_close]   "close",
+  [SYS_trace]   "trace",
+  [SYS_getprocs] "getprocs",
+  [SYS_getprocsinfo] "getprocsinfo",
+  [SYS_getsyscount] "getsyscount",
+};
 
 /*
  * Fetch the int at addr from the current process.
@@ -107,41 +139,37 @@ extern int sys_close(void);
 extern int sys_trace(void);
 extern int sys_getprocs(void);
 extern int sys_getprocsinfo(void);
-
-
-/* syscall nueva del ENTREGABLE 3 */
 extern int sys_getsyscount(void);
 
 /*
  * Tabla de syscalls
  */
 static int (*syscalls[])(void) = {
-[SYS_fork]    sys_fork,
-[SYS_exit]    sys_exit,
-[SYS_wait]    sys_wait,
-[SYS_pipe]    sys_pipe,
-[SYS_read]    sys_read,
-[SYS_kill]    sys_kill,
-[SYS_exec]    sys_exec,
-[SYS_fstat]   sys_fstat,
-[SYS_chdir]   sys_chdir,
-[SYS_dup]     sys_dup,
-[SYS_getpid]  sys_getpid,
-[SYS_sbrk]    sys_sbrk,
-[SYS_sleep]   sys_sleep,
-[SYS_uptime]  sys_uptime,
-[SYS_open]    sys_open,
-[SYS_write]   sys_write,
-[SYS_mknod]   sys_mknod,
-[SYS_unlink]  sys_unlink,
-[SYS_link]    sys_link,
-[SYS_mkdir]   sys_mkdir,
-[SYS_close]   sys_close,
-[SYS_trace]        sys_trace,
-[SYS_getprocs]     sys_getprocs,
-[SYS_getprocsinfo] sys_getprocsinfo,
-/* syscall ENTREGABLE 3 */
-[SYS_getsyscount] sys_getsyscount,
+  [SYS_fork]    sys_fork,
+  [SYS_exit]    sys_exit,
+  [SYS_wait]    sys_wait,
+  [SYS_pipe]    sys_pipe,
+  [SYS_read]    sys_read,
+  [SYS_kill]    sys_kill,
+  [SYS_exec]    sys_exec,
+  [SYS_fstat]   sys_fstat,
+  [SYS_chdir]   sys_chdir,
+  [SYS_dup]     sys_dup,
+  [SYS_getpid]  sys_getpid,
+  [SYS_sbrk]    sys_sbrk,
+  [SYS_sleep]   sys_sleep,
+  [SYS_uptime]  sys_uptime,
+  [SYS_open]    sys_open,
+  [SYS_write]   sys_write,
+  [SYS_mknod]   sys_mknod,
+  [SYS_unlink]  sys_unlink,
+  [SYS_link]    sys_link,
+  [SYS_mkdir]   sys_mkdir,
+  [SYS_close]   sys_close,
+  [SYS_trace]   sys_trace,
+  [SYS_getprocs] sys_getprocs,
+  [SYS_getprocsinfo] sys_getprocsinfo,
+  [SYS_getsyscount] sys_getsyscount,
 };
 
 /*
@@ -156,17 +184,36 @@ syscall(void)
   num = p->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
 
-    /*
-     * ================================
-     * CONTADOR DE SYSCALLS
-     * ================================
-     */
+    /* Contador de syscalls */
     syscall_count[num]++;
 
-    /*
-     * Aquí puede estar el código de TRACE
-     * que haya hecho tu compañero (NO se rompe)
-     */
+    /* ================================
+     * TRACE (ENTREGABLE 1)
+     * ================================ */
+    if(trace_active){
+      cprintf("syscall %s(", syscall_names[num]);
+
+      if(num == SYS_read || num == SYS_write){
+        int fd, n;
+        argint(0, &fd);
+        argint(2, &n);
+        cprintf("fd=%d, n=%d", fd, n);
+      }
+      else if(num == SYS_open){
+        char *path;
+        int mode;
+        argstr(0, &path);
+        argint(1, &mode);
+        cprintf("path=%p, mode=%d", path, mode);
+      }
+      else if(num == SYS_exec){
+        char *path;
+        argstr(0, &path);
+        cprintf("path=%p", path);
+      }
+
+      cprintf(")\n");
+    }
 
     p->tf->eax = syscalls[num]();
   } else {
